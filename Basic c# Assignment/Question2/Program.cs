6b7532963ba008b1b58da7bbc3adf1d49b51b49c
Yash Mundhe <yashmundhe16@gmail.com>
2025-06-17 16:12:30 +0530

    Add files via upload

---
+++ Basic c# Assignment/Question2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Five Numbers : ");
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int n3 = int.Parse(Console.ReadLine());
+                int n4 = int.Parse(Console.ReadLine());
+                int n5 = int.Parse(Console.ReadLine());
+
+                int avg = (n1 + n2 + n3 + n4 + n5) / 5;
+                Console.WriteLine($"Average is {avg}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please Enter inetger only !!");
+            }
+        }
+    }
+}
---
+++ Basic c# Assignment/Question3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Five Numbers : ");
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int n3 = int.Parse(Console.ReadLine());
+                int n4 = int.Parse(Console.ReadLine());
+                int n5 = int.Parse(Console.ReadLine());
+
+                int sum = n1 + n2 + n3 + n4 + n5;
+                Console.WriteLine($"Sum is {sum}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please Enter inetger only !!");
+            }
+        }
+    }
+}
