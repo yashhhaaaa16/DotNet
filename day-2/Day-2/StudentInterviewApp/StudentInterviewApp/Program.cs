774576a93edbeea809753e59460b3541c677a6bf
Yash Mundhe <yashmundhe16@gmail.com>
2025-06-18 13:53:16 +0530

    Add files via upload

---
+++ day-2/Day-2/StudentInterviewApp/StudentInterviewApp/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace StudentInterviewApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome To Interview Application!");
+		try
+		{
+			Student student = new Student();
+            student.OnSelection += new InterviewBasket(OracleDb);
+            student.OnRejection += SqlServerDb;
+            student.Id = 2389;
+			student.ContactName = "Alisha C.";
+			student.City = "Mumbai";
+			student.TotalMarks = 98;
+            if (student.TotalMarks>95)
+            {
+                student.OnSelection += ExcelDb;
+            }
+            else
+            {
+                student.OnSelection -= ExcelDb;
+            }
+                string companyName = "Infoway";
+            Console.WriteLine(student.CalculateResult(student.TotalMarks,ref companyName));
+            Console.WriteLine(companyName);
+			student.CompaniesList("Maharashtra", "Infosys", "IBM");
+            student.CompaniesList("Karnataka", "Bajaj", "Synechron","Infosys","TCS");
+        }
+		catch (Exception ex)
+		{
+            Console.WriteLine(ex.Message);
+		}
+    }
+    private static void OracleDb(string value)
+    {
+        Console.WriteLine($"Selected {value} has been stored in Oracle Database!");
+    }
+    private static void ExcelDb(string value)
+    {
+        Console.WriteLine($"Selected {value} has been stored in Excel Database!");
+    }
+    private static void SqlServerDb(string value)
+    {
+        Console.WriteLine($"Rejected {value} has been stored in Microsoft SQL Server Database!");
+    }
+}
---
+++ day-2/Day-2/StudentInterviewApp/StudentInterviewApp/Student.cs
@@ -0,0 +1,45 @@
+﻿namespace StudentInterviewApp;
+public delegate void InterviewBasket(string name);
+internal class Student
+{
+    public event InterviewBasket OnSelection;
+    public event InterviewBasket OnRejection;
+    public int Id { get; set; }
+    public string ContactName { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public int TotalMarks { get; set; }
+
+    //public string CalculateResult(int totalMarks,out string companyName)
+    public string CalculateResult(int totalMarks, ref string companyName)
+    {
+        if (totalMarks > 80)
+        {
+            companyName = "Synechron";
+            if (OnSelection!=null)
+            {
+                OnSelection($"ContactName - {ContactName} - Company {companyName}");
+            }
+            
+            return $"{ContactName} has been selected for Hr Round!";
+        }
+        else
+        {
+            if (OnRejection != null)
+            {
+                OnRejection($"ContactName - {ContactName} - Company {companyName}");
+            }
+            //companyName = "No Company Name";
+            return $"{ContactName} has been Rejected for Hr Round!";
+        }
+    }
+    public void CompaniesList(string state, params string[] companyNames)
+    {
+        Console.WriteLine($"From State - {state} - There is/are companies - ");
+        for (int i = 0; i < companyNames.Length; i++)
+        {
+            Console.Write($"{companyNames[i]} , ");
+        }
+        Console.WriteLine("");
+    }
+
+}
