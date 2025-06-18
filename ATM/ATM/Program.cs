561f19f213b55dc0b16f79793637cf9fa1e27967
Yash Mundhe <yashmundhe16@gmail.com>
2025-06-18 15:47:35 +0530

    Add files via upload

---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ATM;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        try
+        {
+            Transaction transaction = new();
+
+            Console.WriteLine("---- Enter Customer Details ----");
+
+            Console.WriteLine("Enter Card Number ( Must be 5 digits ) :");
+            transaction.cardNo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Pin ( Must be 4 digits ) :");
+            transaction.pin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter withdrawal amount :");
+            long amt = long.Parse(Console.ReadLine());
+            transaction.WithdrawalAmt = amt;
+
+            Console.WriteLine("Enter Mobile No :");
+            transaction.mobileNo = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("Do you want to register your Mob No ( Y/N ) :");
+            char mn = char.Parse(Console.ReadLine());
+
+            if (mn == 'Y')
+            {
+                transaction.OnSMS += MobSMS;
+            }
+            else
+            {
+                transaction.OnSMS -= MobSMS;
+            }
+
+            Console.WriteLine("Do you want to print slip ( Y/N ) :");
+            char ps = char.Parse(Console.ReadLine());
+
+            if (ps == 'Y')
+            {
+                transaction.OnPrint += PrintSlip;
+            }
+            else
+            {
+                transaction.OnPrint -= PrintSlip;
+            }
+
+            Console.WriteLine(transaction.withdrawal(amt));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    private static void MobSMS(string value)
+    {
+        Console.WriteLine($"Mob - {value} has withdrawal successfully from your account");
+    }
+    private static void PrintSlip(string value)
+    {
+        Console.WriteLine($"Printing Slip - {value} has been debited ");
+    }
+}
---
+++ ATM/ATM/Transaction.cs
@@ -0,0 +1,37 @@
+﻿namespace ATM;
+public delegate void atmBasket(string name);
+internal class Transaction
+{
+    public event atmBasket OnSMS;
+    public event atmBasket OnPrint;
+    private long fixedDeposit = 500000;
+    public int cardNo { get; set; }
+    public int pin { get; set; }
+    public long WithdrawalAmt { get; set; }
+    public long mobileNo { get; set; }
+
+    public string withdrawal(long withAmt) 
+    {
+        try
+        {
+            if (withAmt > fixedDeposit)
+            {
+                throw new Exception("Insufficient Balance");
+            }
+            if (OnSMS != null)
+            {
+                OnSMS($"{WithdrawalAmt}");
+            }
+            if (OnPrint != null)
+            {
+                OnPrint($"{WithdrawalAmt}");
+            }
+            //fixedDeposite -= withAmt;
+            return $"{withAmt} amount Withdraw Successfully";
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+}
