5d718cad9d10b8de39dcc7d4f5b734e2d08ae2b3
Yash Mundhe <yashmundhe16@gmail.com>
2025-06-18 16:56:18 +0530

    Add files via upload

---
+++ Basic c# Assignment/Question20/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Question20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number of terms : ");
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, c = 0;
+            Console.WriteLine($"{a}\n");
+            Console.WriteLine($"{b}\n");
+
+            for (int i = 0; i < n-2; i++)
+            {
+                c = a + b;
+                Console.WriteLine($"{c}\n");
+                a = b; b = c;
+            }
+        }
+    }
+}
