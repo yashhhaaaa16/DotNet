f595b4c401baf0144c20412e29bff92d5ffe1a8f
Yash Mundhe <yashmundhe16@gmail.com>
2025-06-18 17:05:10 +0530

    Add files via upload

---
+++ Basic c# Assignment/Question22/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Question22;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int day, month, year;
+
+        GetDate(out day, out month, out year);
+
+        Console.WriteLine($"\nDate - {day}/{month}/{year}");
+
+    }
+
+    static void GetDate(out int day, out int month, out int year)
+    {
+        Console.Write("Enter day: ");
+        day = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter month: ");
+        month = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter year: ");
+        year = int.Parse(Console.ReadLine());
+    }
+}
