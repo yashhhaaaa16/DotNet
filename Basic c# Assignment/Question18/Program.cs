ed7a299519db858ae96af4fd7a67202b7314b453
Yash Mundhe <yashmundhe16@gmail.com>
2025-06-18 16:27:13 +0530

    Add files via upload

---
+++ Basic c# Assignment/Question18/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Question18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                for(int j = 1; j <= 3; j++)
+                {
+                    for(int k = 1; k <= 3; k++)
+                    {
+                        Console.WriteLine($"{i} {j} {k}");
+                    }
+                }
+            }
+        }
+    }
+}
