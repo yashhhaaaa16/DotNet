b93bc56b534b7cfe3b786294e848d528a7a2fa89
Yash Mundhe <yashmundhe16@gmail.com>
2025-06-18 16:42:54 +0530

    Add files via upload

---
+++ Basic c# Assignment/Question19/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Question19;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Boolean b;
+        do
+        {
+            Console.WriteLine("Enter a Number : ");
+            int num = int.Parse(Console.ReadLine());
+            b = isPrime(num);
+
+        }
+        while (b);
+
+        
+    }
+    public static bool isPrime(int num) 
+    {
+        Boolean ch = check(num);
+
+        if (ch)
+        {
+            int i;
+
+            for (i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                    break;
+            }
+            if (i == num)
+            {
+                Console.WriteLine($"{num} is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not a Prime Number");
+            }
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+
+    public static bool check(int num) 
+    {
+        if (num < 0)
+        {
+            Console.WriteLine($"{num} is a Negative Number \n");
+            Console.WriteLine("Please re-enter the Number ");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+}
