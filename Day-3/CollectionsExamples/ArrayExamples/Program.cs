fead12d809c95bda9ea84e55bdbdbf21d93c8c64
Yash Mundhe <yashmundhe16@gmail.com>
2025-06-20 13:50:00 +0530

    Add files via upload

---
+++ Day-3/CollectionsExamples/ArrayExamples/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ArrayExamples;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Array Examples!");
+        string[] names = new string[] { "First", "Second", "Third" };
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+        Console.WriteLine("");
+        int[,] numbers = new int[2, 2] { { 0, 1 }, { 1, 1 } };
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Console.WriteLine($"{numbers[i, j]}");
+            }
+        }
+        Console.WriteLine("");
+        int[][] marks = new int[3][];
+        marks[0] = new int[3] { 100, 200, 300 };
+        marks[1] = new int[5] { 400, 500, 600, 700, 800 };
+        marks[2] = new int[2] { 1001, 1002 };
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"Row - {i} ");
+            for (int j = 0; j < marks[i].Length; j++)
+            {
+                Console.Write($"\t{marks[i][j]}");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Day-3/CollectionsExamples/StronglyTypedCollectionExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace StronglyTypedCollectionExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Why should you not use weakly typed collections! Because of Boxing and Unboxing Problem!");
+            //ArrayList items=new ArrayList();
+            //items.Add(100);
+            //int firstItem = items[0];
+            List<string> items= new List<string>();
+            items.Add("First");
+            items.Add("Second");
+            items.Add("Third");
+            //string firstItem = items[0];
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("");
+            Stack<string> books=new Stack<string>();
+            books.Push("Book-1");
+            books.Push("Book-2");
+            books.Push("Book-3");
+
+            Console.WriteLine(books.Peek());
+
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
