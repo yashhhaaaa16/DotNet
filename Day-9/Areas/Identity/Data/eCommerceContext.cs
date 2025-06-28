2bf931bd6fb90342828af0761e863028a8e3b9a7
Yash Mundhe <yashmundhe16@gmail.com>
2025-06-28 17:54:01 +0530

    Add files via upload

---
+++ Day-9/Areas/Identity/Data/eCommerceContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Areas.Identity.Data;
+
+public class eCommerceContext : IdentityDbContext<IdentityUser>
+{
+    public eCommerceContext(DbContextOptions<eCommerceContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
